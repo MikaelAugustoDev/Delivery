3edcfee8152f6dbe573a2fdbc7eb8be8a965821b
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-06-18 00:40:09 -0300

    Configurando Dependencias do projeto

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5173/",
+    screenshotOnRunFailure: false,
+    video: false,
+    videoUploadOnPasses: false,
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
