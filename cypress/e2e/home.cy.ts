5d46ef0d10fe31df7db1c6606d81d7eeb5b7521f
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-06-18 23:36:45 -0300

    Fazendo alguns testes na pagina Home

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,46 @@
+
+describe("Testando a pagina Home", () => {
+    it("Redirecionando para a rota Cardapio", () => {
+        cy.visit("/");
+        cy.contains("Cardápio").click();
+        cy.url().should("include", "/cardapio");
+    })
+
+    it("Redirecionando para a rota Contato", () => {
+        cy.visit("/");
+        cy.contains("Contato").click();
+        cy.url().should("include", "/contato");
+    })
+})
+
+describe("Testando Rotas relacionadas a Cadastro e Login", () => {
+    it("Redirecionando para Rota Login", () => {
+        cy.visit("/");
+        cy.contains("Fazer Login").click();
+        cy.url().should("include", "/login");
+    })
+
+    it("Existe um botao de Entrar no meu Login?", () => {
+        cy.visit("/login");
+        cy.contains("Entrar")
+    })
+
+    it("Redirecionando para Rota Esqueci minha senha", () => {
+        cy.visit("/login");
+        cy.contains("Clique aqui").click();
+        cy.url().should("include", "/esqueceuasenha");
+    })
+
+    it("Redirecionando para Rota Cadastro", () => {
+        cy.visit("/login");
+        cy.contains("Quero me Cadastrar").click();
+        cy.url().should("include", "/cadastro");
+        cy.get(".sc-fGZLLs > img").click();
+        cy.url().should("include", "/login");
+    })
+
+    it("Existe um botao de Entrar no meu Login?", () => {
+        cy.visit("/cadastro");
+        cy.contains("Cadastrar")
+    })
+}) 
